b0b8c88991c5ffc03f004562b0bd573cb9d8b250
Blake Lundquist <blake.lundquist@hey.com>
2024-05-16 14:23:46 +0200

    add saving

--- src/editor.rs
+++ src/editor.rs
@@ -56,7 +56,7 @@ impl Editor {
     }
     pub fn default() -> Self {
         let args: Vec<String> = env::args().collect();
-        let mut initial_status = String::from("HELP: Ctrl-C = quit");
+        let mut initial_status = String::from("HELP: Ctrl-S = save | Ctrl-C = quit");
         let document = if args.len() > 1 {
             let file_name = &args[1];
             let doc = Document::open(&file_name);
@@ -105,6 +105,15 @@ impl Editor {
         match pressed_key {
             Key::Ctrl('c') => {
                 self.should_quit = true;
+            }
+            Key::Ctrl('s') => {
+                if self.document.save().is_ok() {
+                    self.status_message = StatusMessage::from(
+                        "File saved successfully.".to_string()
+                    );
+                } else {
+                    self.status_message = StatusMessage::from("Error writing file!".to_string());
+                }
             }
             Key::Char(c) => {
                 self.document.insert(&self.cursor_position, c);
